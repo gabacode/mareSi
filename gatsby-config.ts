6aecf1eebbbe9a392cb1ab9c7d25bddeaf631921
Gabriele Scalici <gaba41@gmail.com>
2022-04-16 19:55:25 +0200

    add contact form

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,7 @@
 import type { GatsbyConfig } from 'gatsby';
-
+require('dotenv').config({
+  path: `.env`,
+});
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `MareSì`,
--- src/components/landing-page/Form.tsx
+++ src/components/landing-page/Form.tsx
@@ -1,12 +1,18 @@
 import React, { ChangeEvent, useState } from 'react';
 import { Box, Button, Input, Textarea } from 'theme-ui';
+import axios from 'axios';
 interface FormProps {
-  name: string;
+  subject: string;
   email: string;
-  message: string;
+  text: string;
 }
+const initialFields: FormProps = {
+  subject: '',
+  email: '',
+  text: '',
+};
 export const Form = () => {
-  const [fields, setFields] = useState({} as FormProps);
+  const [fields, setFields] = useState(initialFields);
   const [sending, setSending] = useState(false);
   const handleInput = (
     input: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -17,6 +23,30 @@ export const Form = () => {
       [name]: value,
     });
   };
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    setSending(true);
+    try {
+      await axios
+        .post(process.env.GATSBY_MAIL_ENDPOINT ?? '', {
+          ...fields,
+        })
+        .then(res => {
+          if (res.status === 200) {
+            setFields(initialFields);
+            window.alert('Messaggio Inviato!');
+            setSending(false);
+          }
+        });
+    } catch (error) {
+      setFields(initialFields);
+      window.alert(error);
+      setSending(false);
+    }
+  };
+
   return (
     <Box
       bg="white"
@@ -26,11 +56,11 @@ export const Form = () => {
         padding: [4, 4, 4],
       }}
     >
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input
           type="text"
-          name="name"
-          value={fields.name}
+          name="subject"
+          value={fields.subject}
           placeholder="Nome"
           onChange={handleInput}
           required
@@ -46,12 +76,13 @@ export const Form = () => {
         <Textarea
           rows={8}
           cols={30}
-          name="message"
-          value={fields.message}
+          name="text"
+          value={fields.text}
           onChange={handleInput}
           placeholder="Why do you want join? Tell us about your love for the sea!"
         />
         <Button
+          bg={sending ? 'lightgray' : 'primary'}
           sx={{
             float: 'right',
             cursor: 'pointer',
